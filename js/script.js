da2de9d37aa1ac55d382ccb65f2e78a06532dc70
Delphine Oletha <Delphineolethaa@gmail.com>
2024-06-20 22:42:41 +0700

    .

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function conversion(){
+    let inputNumber = document.getElementById("input-number")
+    let outputNumber = document.getElementById("Converted")
+    console.log(inputNumber);(outputNumber);
+}
+
+function reverse(e){
+    console.log("its clicked")
+}
+
+function reset(e){
+    console.log("its clicked")
+}
+
